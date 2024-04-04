5a05a89ac8c5b880de4be10b317c7cde3b2e4443
mg0806 <manohargupta0806@gmail.com>
2024-04-04 14:31:35 +0530

    update stripe-webhooks.ts

--- pages/api/stripe-webhooks.ts
+++ pages/api/stripe-webhooks.ts
@@ -36,7 +36,6 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
         return res.status(400).send('WEBHOOK ERROR' + err)
         
     }
-    console.log("#######"+event);
     
     switch(event.type){
         case 'charge.succeeded':
